45b9f068a5d1348b67f4f555b88858fc796c0109
winux <wnxgeek@icloud.com>
2024-06-17 23:40:06 +0300

    2024.06.17_23

---
+++ _my_scripts/regex.py
@@ -0,0 +1,25 @@
+import re
+import rich
+
+
+regex = r"(\S+) +(\S+)"
+line = 'ip address 10.0.13.1 255.255.255.0'
+m = re.search(regex, line)
+
+print (m.group())
+print (m.group(2))
+print (m.span())
+
+# rich.inspect(m, methods=True)
+
+print (m.groups())
+
+regex = r"ip address (?P<ip>\S+) +(?P<mask>\S+)"
+m = re.search(regex, line)
+print (m.group())
+
+print(m.groups())
+
+print(m.groupdict())
+
+print (m.group("ip"))
